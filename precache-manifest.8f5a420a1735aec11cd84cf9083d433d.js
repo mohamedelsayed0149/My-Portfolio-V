42cd34e991f3c15ba887a7e3d86d5e9d77461e12
mohamedelsayed0149 <mohamed11490@gmail.com>
2021-02-09 18:42:50 +0200

    Updates

---
+++ precache-manifest.8f5a420a1735aec11cd84cf9083d433d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bd3cdc54240679da9bb06c683a60bb",
+    "url": "/My-Portfolio-V/index.html"
+  },
+  {
+    "revision": "d224ff2ddc93555d2317",
+    "url": "/My-Portfolio-V/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a994cad431dd34d36829",
+    "url": "/My-Portfolio-V/static/js/2.8ee0ffd0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/My-Portfolio-V/static/js/2.8ee0ffd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d224ff2ddc93555d2317",
+    "url": "/My-Portfolio-V/static/js/main.9425b5a6.chunk.js"
+  },
+  {
+    "revision": "7b8f3d4c5b1a9b25ac8a",
+    "url": "/My-Portfolio-V/static/js/runtime-main.c1c0cab7.js"
+  }
+]);
